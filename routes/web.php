dbf30168e9b448e9d526a8a75ec0af988301a03f
Mohammed Naji <m.naji1512@gmail.com>
2023-02-14 19:00:20 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,66 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 // .
 // =>
 // ::
+
+// use , namespace
+
+// Route::get('url', 'Actions');
+// Route::post('url', 'Actions');
+// Route::put('url', 'Actions');
+// Route::patch('url', 'Actions');
+// Route::delete('url', 'Actions');
+
+
+// https://www/mohamednaji.com/about
+
 Route::get('/', function() {
-    return 'Homepage';
+    return 'Homepage - Get';
 });
+
+Route::post('/', function() {
+    return 'Homepage - Post';
+});
+
+Route::put('/', function() {
+    return 'Homepage - Put';
+});
+
+Route::get('about', function() {
+    return 'About Page';
+});
+
+Route::get('team', function() {
+    return 'Team Page';
+});
+
+Route::get('contact', function() {
+    return 'Contact Page';
+});
+
+Route::get('services', function() {
+    return 'Services Page';
+});
+
+Route::get('user/{name}', function($name) {
+    return 'User ' . $name;
+});
+
+// Route::get('news', function() {
+//     return 'News';
+// });
+
+Route::get('news/{id?}', function($id = '') {
+    return 'News ' . $id;
+});
+
+Route::prefix('admin')->group(function() {
+    Route::get('home', function() { return 'Admin Home'; });
+    Route::get('posts', function() { return 'Admin Posts'; });
+    Route::get('comments', function() { return 'Admin Comments'; });
+    Route::get('products', function() { return 'Admin Products'; });
+});
+
